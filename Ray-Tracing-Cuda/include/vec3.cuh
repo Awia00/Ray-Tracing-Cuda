eecb17d239287188248b443617387577d83a77d2
Anders Wind <awia00@gmail.com>
2019-02-24 17:17:56 +0100

    Finished vec3

--- Ray-Tracing-Cuda/include/vec3.cuh
+++ Ray-Tracing-Cuda/include/vec3.cuh
@@ -9,20 +9,54 @@ struct vec3 {
 		e[2] = e2;
 	}
 
-	// unary operators 
+	// unary operators
 	inline const vec3& operator+() const { return *this; }
 	inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 	inline float operator[](int i) const { return e[i]; }
 	inline float& operator[](int i) { return e[i]; }
 
 	// binary operators
-	inline vec3& operator+=(const vec3 &v2);
-	inline vec3& operator-=(const vec3 &v2);
-	inline vec3& operator*=(const vec3 &v2);
-	inline vec3& operator/=(const vec3 &v2);
-	inline vec3& operator*=(const float t);
-	inline vec3& operator/=(const float t);
+	inline vec3& operator+=(const vec3 &v) {
+		e[0] += v.e[0];
+		e[1] += v.e[1];
+		e[2] += v.e[2];
+		return *this;
+	}
 
+	inline vec3& operator-=(const vec3 &v) {
+		e[0] -= v.e[0];
+		e[1] -= v.e[1];
+		e[2] -= v.e[2];
+		return *this;
+	}
+
+	inline vec3& operator*=(const vec3 &v) {
+		e[0] *= v.e[0];
+		e[1] *= v.e[1];
+		e[2] *= v.e[2];
+		return *this;
+	}
+
+	inline vec3& operator/=(const vec3 &v) {
+		e[0] /= v.e[0];
+		e[1] /= v.e[1];
+		e[2] /= v.e[2];
+		return *this;
+	}
+
+	inline vec3& operator*=(const float t) {
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	inline vec3& operator/=(const float t) {
+		e[0] /= t;
+		e[1] /= t;
+		e[2] /= t;
+		return *this;
+	}
 
 	inline float length() const {
 		return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
@@ -32,8 +66,65 @@ struct vec3 {
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
 
-	inline void make_unit_vector();
+	inline void make_unit_vector() {
+		float k = 1.0f / length();
+		e[0] *= k;
+		e[1] *= k;
+		e[2] *= k;
+	}
 };
+
+inline vec3 operator+(const vec3 & v1, const vec3 & v2) {
+	return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[0]);
+}
+
+inline vec3 operator-(const vec3 & v1, const vec3 & v2) {
+	return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[0]);
+}
+
+inline vec3 operator*(const vec3 & v1, const vec3 & v2) {
+	return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[0]);
+}
+
+inline vec3 operator/(const vec3 & v1, const vec3 & v2) {
+	return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[0]);
+}
+
+inline vec3 cross(const vec3 & v1, const vec3 & v2) {
+	return vec3(
+		v1[1] * v2[2] - v1[2] * v2[1],
+		v1[0] * v2[2] - v1[2] * v2[0],
+		v1[0] * v2[1] - v1[1] * v2[0]
+	);
+}
+
+inline float dot(const vec3 & v1, const vec3 & v2) {
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[0];
+}
+
+inline vec3 operator*(const vec3 & v1, const float t) {
+	return vec3(v1[0] * t, v1[1] * t, v1[2] * t);
+}
+
+inline vec3 operator/(const vec3 & v1, const float t) {
+	return vec3(v1[0] / t, v1[1] / t, v1[2] / t);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
+inline std::istream& operator>>(std::istream& is, vec3 &t) {
+	is >> t.e[0] >> t.e[1] >> t.e[2];
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream& is, vec3 &t) {
+	is << t.e[0] << " " << t.e[1] << " " << t.e[2];
+	return is;
+}
+
+// Specific versions
 
 struct RGB : public vec3 {
 	inline float r() const { return e[0]; }
